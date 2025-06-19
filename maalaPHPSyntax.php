971be2dbca5fc3a5ea4cd6e7d3c96aeea8bb518e
webdev2025jaymartmaala <webdev2025jaymartmaala@gmail.com>
2025-06-19 20:37:46 +0800

    Save maalaActivity1

---
+++ maalaPHPSyntax.php
@@ -0,0 +1,93 @@
+<?php
+echo "Jaymart C. Maala";
+echo "<br>";
+echo 'Jaymart C. Maala';
+echo "<hr>";
+
+//2. Setting Variable
+$maalaClass = "Web Development NCIII";
+echo $maalaClass;
+echo '<br>maalaClass';
+echo "<br>$maalaClass";
+echo "<hr>";
+
+//3. Setting the variable with operation
+// addition, Subtraction, Multiplication, Division
+$maalaNum1 = 15;
+$maalaNum2 = 30;
+$maalaAllowance = 5532;
+
+// addition
+$maalaSum = $maalaAllowance + $maalaNum1;
+echo "ADDITION";
+echo "<br>";
+echo "Allawance: $maalaAllowance";
+echo "<br>";
+echo "Num1: $maalaNum1";
+echo "<br>";
+echo "ADDITION: $maalaSum";
+echo "<br>";
+echo "<hr>";
+
+//subtraction
+$maalaDifference = $maalaAllowance - $maalaNum2;
+echo "SUBTRACTION";
+echo "<br>";
+echo "Allowance: $maalaAllowance";
+echo "<br>";
+echo "Num1: $maalaNum1";
+echo "<br>";
+echo "SUBTRACTION: $maalaDifference";
+echo "<br>";
+echo "<hr>";
+
+//multiplication
+$maalaProduct = $maalaAllowance * $maalaNum1;
+echo "MULTIPLICATION";
+echo "<br>";
+echo "Allowance: $maalaAllowance";
+echo "<br>";
+echo "Num1: $maalaNum1";
+echo "<br>";
+echo "MULTIPLICATION: $maalaProduct";
+echo "<br>";
+echo "<hr>";
+
+//division
+$maalaQuotient = $maalaAllowance / $maalaNum2;
+echo "DIVISION";
+echo "<br>";
+echo "Allowance: $maalaAllowance";
+echo "<br>";
+echo "Num1: $maalaNum2";
+echo "<br>";
+echo "DIVISION: $maalaProduct";
+echo "<br>";
+echo "<hr>";
+
+$maalaHobby = "Programming";
+echo "I Like $maalaHobby!";
+echo "<br>";
+echo "<hr>";
+
+$maalaVar1 = "Jaymart";
+$maalaVar2 = "Castro";
+$maalaVar3 = "Maala";
+
+echo "$maalaVar1 $maalaVar2 $maalaVar3";
+echo "<hr>";
+
+$maalaVar1 = "Jaymart";
+$maalaVar2 = "Castro";
+$maalaVar3 = "Maala";
+
+//$maalaVar1 = $maalaVar2;
+$maalaVar2 = $maalaVar1;
+$maalaVar3 = $maalaVar1;
+
+echo "$maalaVar1 <br>";
+echo "$maalaVar2 <br>";
+echo "$maalaVar3 <br>";
+
+
+?>
